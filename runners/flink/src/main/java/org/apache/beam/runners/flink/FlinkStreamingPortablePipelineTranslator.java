b001edb830ff3c4fd43995632de9999b3922e811
Thomas Weise <thw@apache.org>
2018-08-16 18:49:23 -0700

    [STRM-1271] [LYFT] Add custom translator for Flink Kafka source.

--- runners/flink/src/main/java/org/apache/beam/runners/flink/FlinkStreamingPortablePipelineTranslator.java
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/FlinkStreamingPortablePipelineTranslator.java
@@ -176,6 +176,11 @@ public class FlinkStreamingPortablePipelineTranslator
         PTransformTranslation.CREATE_VIEW_TRANSFORM_URN,
         (String id, RunnerApi.Pipeline pipeline, StreamingTranslationContext context) -> {});
 
+    //LYFT CUSTOM
+    LyftFlinkStreamingPortableTranslations customTranslations =
+        new LyftFlinkStreamingPortableTranslations();
+    customTranslations.addTo(translatorMap);
+
     this.urnToTransformTranslator = translatorMap.build();
   }
 
