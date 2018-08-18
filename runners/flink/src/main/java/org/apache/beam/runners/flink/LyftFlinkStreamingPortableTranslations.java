7f95a19cb1316d2b8911cdb77bc3863523087a5b
Thomas Weise <thw@apache.org>
2018-08-18 00:06:39 -0700

    [STRM-1271] [LYFT] Add custom translator for Flink Kinesis source.

--- runners/flink/src/main/java/org/apache/beam/runners/flink/LyftFlinkStreamingPortableTranslations.java
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/LyftFlinkStreamingPortableTranslations.java
@@ -35,6 +35,8 @@ import org.apache.beam.sdk.util.WindowedValue;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
+import org.apache.flink.streaming.connectors.kinesis.FlinkKinesisConsumer;
+import org.apache.flink.streaming.connectors.kinesis.serialization.KinesisDeserializationSchema;
 import org.apache.flink.streaming.util.serialization.KeyedDeserializationSchema;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -48,6 +50,7 @@ public class LyftFlinkStreamingPortableTranslations {
       ImmutableMap.Builder<String, PTransformTranslator<StreamingTranslationContext>>
           translatorMap) {
     translatorMap.put("lyft:flinkKafkaInput", this::translateKafkaInput);
+    translatorMap.put("lyft:flinkKinesisInput", this::translateKinesisInput);
   }
 
   private void translateKafkaInput(
@@ -91,7 +94,6 @@ public class LyftFlinkStreamingPortableTranslations {
     private static final long serialVersionUID = -1L;
 
     private final TypeInformation<WindowedValue<byte[]>> ti;
-    long cnt = 0;
 
     public ByteArrayWindowedValueSchema() {
       this.ti =
@@ -107,7 +109,6 @@ public class LyftFlinkStreamingPortableTranslations {
     @Override
     public WindowedValue<byte[]> deserialize(
         byte[] messageKey, byte[] message, String topic, int partition, long offset) {
-      cnt++;
       System.out.println("###Kafka record: " + new String(message, Charset.defaultCharset()));
       return WindowedValue.valueInGlobalWindow(message);
     }
@@ -115,6 +116,73 @@ public class LyftFlinkStreamingPortableTranslations {
     @Override
     public boolean isEndOfStream(WindowedValue<byte[]> nextElement) {
       return false;
+    }
+  }
+
+  private void translateKinesisInput(
+      String id,
+      RunnerApi.Pipeline pipeline,
+      FlinkStreamingPortablePipelineTranslator.StreamingTranslationContext context) {
+    RunnerApi.PTransform pTransform = pipeline.getComponents().getTransformsOrThrow(id);
+
+    String stream;
+    Properties properties = new Properties();
+    ObjectMapper mapper = new ObjectMapper();
+    try {
+      Map<String, Object> params =
+          mapper.readValue(pTransform.getSpec().getPayload().toByteArray(), Map.class);
+
+      Preconditions.checkNotNull(
+          stream = (String) params.get("stream"), "'stream' needs to be set");
+      Map<?, ?> consumerProps = (Map) params.get("properties");
+      Preconditions.checkNotNull(consumerProps, "'properties' need to be set");
+      properties.putAll(consumerProps);
+    } catch (IOException e) {
+      throw new RuntimeException("Could not parse KafkaConsumer properties.", e);
+    }
+
+    logger.info("Kinesis consumer for stream {} with properties {}", stream, properties);
+
+    DataStreamSource<WindowedValue<byte[]>> source =
+        context
+            .getExecutionEnvironment()
+            .addSource(
+                new FlinkKinesisConsumer<>(
+                    stream, new KinesisByteArrayWindowedValueSchema(), properties));
+    context.addDataStream(Iterables.getOnlyElement(pTransform.getOutputsMap().values()), source);
+  }
+
+  /**
+   * Deserializer for native Flink Kafka source that produces {@link WindowedValue} expected by Beam
+   * operators.
+   */
+  private static class KinesisByteArrayWindowedValueSchema
+      implements KinesisDeserializationSchema<WindowedValue<byte[]>> {
+    private static final long serialVersionUID = -1L;
+
+    private final TypeInformation<WindowedValue<byte[]>> ti;
+
+    public KinesisByteArrayWindowedValueSchema() {
+      this.ti =
+          new CoderTypeInformation<>(
+              WindowedValue.getFullCoder(ByteArrayCoder.of(), GlobalWindow.Coder.INSTANCE));
+    }
+
+    @Override
+    public TypeInformation<WindowedValue<byte[]>> getProducedType() {
+      return ti;
+    }
+
+    @Override
+    public WindowedValue<byte[]> deserialize(
+        byte[] recordValue,
+        String partitionKey,
+        String seqNum,
+        long approxArrivalTimestamp,
+        String stream,
+        String shardId)
+        throws IOException {
+      return WindowedValue.valueInGlobalWindow(recordValue);
     }
   }
 }
