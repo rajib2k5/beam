7b468216f702a4699a5274be8847ba2aadd4cce3
Thomas Weise <tweise@lyft.com>
2018-09-26 04:17:49 +0000

    pipeline options debugging

--- runners/flink/src/main/java/org/apache/beam/runners/flink/FlinkExecutionEnvironments.java
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/FlinkExecutionEnvironments.java
@@ -137,6 +137,8 @@ public class FlinkExecutionEnvironments {
       FlinkPipelineOptions options, List<String> filesToStage, @Nullable String confDir) {
 
     LOG.info("Creating a Streaming Environment.");
+    LOG.info("pipelineOptions: {}", options);
+    LOG.info("env: {}", System.getenv());
 
     String masterUrl = options.getFlinkMaster();
     Configuration flinkConfig = getFlinkConfiguration(confDir);
@@ -176,6 +178,12 @@ public class FlinkExecutionEnvironments {
     final int parallelism =
         determineParallelism(
             options.getParallelism(), flinkStreamEnv.getParallelism(), flinkConfig);
+    LOG.info(
+        "Parallelism for {} {} {} is {}",
+        options.getParallelism(),
+        flinkStreamEnv.getParallelism(),
+        flinkConfig,
+        parallelism);
     flinkStreamEnv.setParallelism(parallelism);
     // set parallelism in the options (required by some execution code)
     options.setParallelism(parallelism);
