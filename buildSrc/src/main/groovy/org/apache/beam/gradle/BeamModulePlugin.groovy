28368b721a234ff869ca009b51d850a1d5b8a1ca
Thomas Weise <thw@apache.org>
2018-12-03 19:34:06 -0800

    Commit for release 2.10.0-lyft20181203

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -272,7 +272,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '2.10.0'
+    project.version = '2.10.0-lyft20181203'
     if (!isRelease(project)) {
       project.version += '-SNAPSHOT'
     }
