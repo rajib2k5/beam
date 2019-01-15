3ced6d92f791f6c3601fe63693adfe20a70007a4
Maximilian Michels <mxm@apache.org>
2019-01-15 12:56:24 -0500

    [BEAM-6440] Fix leakage of timer de-duplication map
    The FlinkStateInternals use a keyed map of pending timers to make up for Flink's
    limitation to only be able to delete timers with their original timestamp, not
    via timer id.
    
    The Map leaked memory because subclasses of DoFnOperator overwrote `fireTimer`
    which was responsible for performing cleanup in the map upon firing a timer.

--- runners/flink/src/test/java/org/apache/beam/runners/flink/translation/wrappers/streaming/StreamRecordStripper.java
+++ runners/flink/src/test/java/org/apache/beam/runners/flink/translation/wrappers/streaming/StreamRecordStripper.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.beam.runners.flink.streaming;
+package org.apache.beam.runners.flink.translation.wrappers.streaming;
 
 import com.google.common.base.Function;
 import com.google.common.collect.FluentIterable;
