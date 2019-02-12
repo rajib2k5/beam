81a9b2f6c226662b2e348f652c4f023aa50b2569
Thomas Weise <tweise@users.noreply.github.com>
2019-02-11 17:37:46 -0800

    Use loggedAt to replace future occurredAt timestamps (#13)

--- runners/flink/src/test/java/org/apache/beam/runners/flink/LyftFlinkStreamingPortableTranslationsTest.java
+++ runners/flink/src/test/java/org/apache/beam/runners/flink/LyftFlinkStreamingPortableTranslationsTest.java
@@ -17,8 +17,13 @@
  */
 package org.apache.beam.runners.flink;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
 import java.util.Base64;
+import java.util.TimeZone;
+import java.util.zip.Deflater;
 import org.apache.beam.runners.flink.LyftFlinkStreamingPortableTranslations.LyftBase64ZlibJsonSchema;
 import org.apache.beam.sdk.util.WindowedValue;
 import org.junit.Assert;
@@ -59,8 +64,7 @@ public class LyftFlinkStreamingPortableTranslationsTest {
 
   @Test
   public void testBeamKinesisSchemaNoTimestamp() throws IOException {
-    // [{"event_id": 1}]
-    byte[] message = Base64.getDecoder().decode("eJyLrlZKLUvNK4nPTFGyUjCsjQUANv8Fzg==");
+    byte[] message = encode("[{\"event_id\": 1}]");
 
     LyftBase64ZlibJsonSchema schema = new LyftBase64ZlibJsonSchema();
     WindowedValue<byte[]> value = schema.deserialize(message, "", "", 0, "", "");
@@ -85,5 +89,37 @@ public class LyftFlinkStreamingPortableTranslationsTest {
     Assert.assertArrayEquals(message, value.getValue());
     // we should output the oldest timestamp in the bundle
     Assert.assertEquals(1540599602000L, value.getTimestamp().getMillis());
+  }
+
+  @Test
+  public void testBeamKinesisSchemaFutureOccurredAtTimestamp() throws Exception {
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS");
+    sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+    long loggedAtMillis = sdf.parse("2018-10-27 00:10:02.000000").getTime();
+    String events =
+        "[{\"event_id\": 1, \"occurred_at\": \"2018-10-27 00:20:02.900\", \"logged_at\": "
+            + loggedAtMillis / 1000
+            + "}]";
+    byte[] message = encode(events);
+    LyftBase64ZlibJsonSchema schema = new LyftBase64ZlibJsonSchema();
+    WindowedValue<byte[]> value = schema.deserialize(message, "", "", 0, "", "");
+
+    Assert.assertArrayEquals(message, value.getValue());
+    Assert.assertEquals(loggedAtMillis, value.getTimestamp().getMillis());
+  }
+
+  private static byte[] encode(String data) throws IOException {
+    Deflater deflater = new Deflater();
+    deflater.setInput(data.getBytes(Charset.defaultCharset()));
+    deflater.finish();
+    byte[] buf = new byte[4096];
+    try (ByteArrayOutputStream bos = new ByteArrayOutputStream(data.length())) {
+      while (!deflater.finished()) {
+        int count = deflater.deflate(buf);
+        bos.write(buf, 0, count);
+      }
+      return bos.toByteArray();
+    }
   }
 }
